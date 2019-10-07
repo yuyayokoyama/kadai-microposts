0b1a68d52f8bc91d9fbbcd7dce2f1696f09eb079
yuya.yokoyama <potentialability1031@gmail.com>
2019-10-07 14:51:18 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   
   has_secure_password
+  
+  has_many :microposts
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'toppages#index'
   
@@ -9,5 +8,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
     
 end
